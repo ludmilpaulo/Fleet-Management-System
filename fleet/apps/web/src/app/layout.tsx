19a2aa61b210a35934c871a8d87e5449a8beeb7a
Ludmil <ludmil@Ludmils-MacBook-Air.local>
2025-10-13 01:25:56 +0200

    style

--- fleet/apps/web/src/app/layout.tsx
+++ fleet/apps/web/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "@styles/globals.css";
 import { ReduxProvider } from '@/providers/ReduxProvider';
 import { MixpanelProvider } from '@/components/providers/mixpanel-provider';
 
